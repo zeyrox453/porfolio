2964163762c18f406c9f28c743e431b52f18c413
Lucas Gugliardi <lucasgugliardi6@gmail.com>
2026-03-13 15:47:44 +0000

    Correction base URL pour compatibilité multi-hébergeur

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/porfolio/', // Indispensable pour GitHub Pages
+  base: './', // Plus robuste pour VPS + GitHub Pages
 })
